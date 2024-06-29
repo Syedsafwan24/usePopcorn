33640c91bca80c90ad0fe8754eabb7fc234932d7
Syedsafwan24 <136835578+Syedsafwan24@users.noreply.github.com>
2024-06-29 18:08:16 +0530

    cleanup func for title

--- src/App.js
+++ src/App.js
@@ -336,6 +336,10 @@ useEffect(
   function () {
     if (!title) return;
     document.title = `MOVIE | ${title}`;
+
+    return function () {
+      document.title = " usePopcorn";
+    };
   },
   [title]
 );
